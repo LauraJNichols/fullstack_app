b86450e6d6929e22bd2bc7373e3e68a1b49f5cbe
LauraCNichols <laura.caldwellnichols@sky.uk>
2019-11-13 16:30:41 +0000

    Trying to sidebar....

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,173 @@
-// /client/App.js
-import React, { Component } from 'react';
-import Header from './header'
+import React from "react";
+import ReactDOM from "react-dom";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Sidebar from "react-sidebar";
+import MaterialTitlePanel from "./material_title_panel";
+import SidebarContent from "./sidebar_content";
 
-class App extends Component {
-  // initialize our state
-  state = {
-    currentUrl: null,
-  };
+const styles = {
+  contentHeaderMenuLink: {
+    textDecoration: "none",
+    color: "white",
+    padding: 8
+  },
+  content: {
+    padding: "16px"
+  }
+};
 
-  // here is our UI
-  // it is easy to understand their functions when you
-  // see them render into our screen
-  //
-  // MongoDB to Web Page:
-  // If data contains "id" and "message" keys, they will appear. 
-  // If either are missing, they will not appear. Simiarly, any extra keys will not appear or interact with the page. 
-  render() {
-    const { data } = this.state;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      docked: false,
+      open: false,
+      transitions: true,
+      touch: true,
+      shadow: true,
+      pullRight: false,
+      touchHandleWidth: 20,
+      dragToggleDistance: 30
+    };
+
+    this.renderPropCheckbox = this.renderPropCheckbox.bind(this);
+    this.renderPropNumber = this.renderPropNumber.bind(this);
+    this.onSetOpen = this.onSetOpen.bind(this);
+    this.menuButtonClick = this.menuButtonClick.bind(this);
+  }
+
+  onSetOpen(open) {
+    this.setState({ open });
+  }
+
+  menuButtonClick(ev) {
+    ev.preventDefault();
+    this.onSetOpen(!this.state.open);
+  }
+
+  renderPropCheckbox(prop) {
+    const toggleMethod = ev => {
+      const newState = {};
+      newState[prop] = ev.target.checked;
+      this.setState(newState);
+    };
+
     return (
-      <div>
-          
-        <Header/>
-        <div name="How do I put everything here? I want it squished."></div>
+      <p key={prop}>
+        <label htmlFor={prop}>
+          <input
+            type="checkbox"
+            onChange={toggleMethod}
+            checked={this.state[prop]}
+            id={prop}
+          />
+          {prop}
+        </label>
+      </p>
+    );
+  }
 
-      </div>
+  renderPropNumber(prop) {
+    const setMethod = ev => {
+      const newState = {};
+      newState[prop] = parseInt(ev.target.value, 10);
+      this.setState(newState);
+    };
+
+    return (
+      <p key={prop}>
+        {prop}{" "}
+        <input type="number" onChange={setMethod} value={this.state[prop]} />
+      </p>
+    );
+  }
+
+  render() {
+    const sidebar = <SidebarContent />;
+
+    const contentHeader = (
+      <span>
+        {!this.state.docked && (
+          <a
+            onClick={this.menuButtonClick}
+            href="#"
+            style={styles.contentHeaderMenuLink}
+          >
+            =
+          </a>
+        )}
+        <span> React Sidebar</span>
+      </span>
+    );
+
+    const sidebarProps = {
+      sidebar,
+      docked: this.state.docked,
+      sidebarClassName: "custom-sidebar-class",
+      contentId: "custom-sidebar-content-id",
+      open: this.state.open,
+      touch: this.state.touch,
+      shadow: this.state.shadow,
+      pullRight: this.state.pullRight,
+      touchHandleWidth: this.state.touchHandleWidth,
+      dragToggleDistance: this.state.dragToggleDistance,
+      transitions: this.state.transitions,
+      onSetOpen: this.onSetOpen
+    };
+
+    return (
+      <Sidebar {...sidebarProps}>
+        <MaterialTitlePanel title={contentHeader}>
+          <div style={styles.content}>
+            <p>
+              React Sidebar is a sidebar component for React. It offers the
+              following features:
+            </p>
+            <ul>
+              <li>Have the sidebar slide over main content</li>
+              <li>Dock the sidebar next to the content</li>
+              <li>Touch enabled: swipe to open and close the sidebar</li>
+              <li>
+                Easy to combine with media queries for auto-docking (
+                <a href="responsive_example.html">see example</a>)
+              </li>
+              <li>
+                Sidebar and content passed in as PORCs (Plain Old React
+                Components)
+              </li>
+              <li>
+                <a href="https://github.com/balloob/react-sidebar">
+                  Source on GitHub
+                </a>{" "}
+                (MIT license)
+              </li>
+              <li>Only dependency is React</li>
+            </ul>
+            <p>
+              <a href="https://github.com/balloob/react-sidebar#installation">
+                Instructions how to get started.
+              </a>
+            </p>
+            <p>
+              <b>Current rendered sidebar properties:</b>
+            </p>
+            {[
+              "open",
+              "docked",
+              "transitions",
+              "touch",
+              "shadow",
+              "pullRight"
+            ].map(this.renderPropCheckbox)}
+            {["touchHandleWidth", "dragToggleDistance"].map(
+              this.renderPropNumber
+            )}
+          </div>
+        </MaterialTitlePanel>
+      </Sidebar>
     );
   }
 }
-
-
 
 export default App;
