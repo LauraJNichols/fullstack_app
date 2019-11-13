2298ecf171268dcf7fe9e732f9d657a574942be0
LauraCNichols <laura.caldwellnichols@sky.uk>
2019-11-13 14:17:08 +0000

    This app works, just before I butcher it...

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const router = express.Router();
 
 // this is our MongoDB database
 const dbRoute =
-  'mongodb+srv://luka:<PASSWORD>@cluster0-croel.mongodb.net/test?retryWrites=true&w=majority';
+  'mongodb+srv://luka:scram@cluster0-croel.mongodb.net/test?retryWrites=true&w=majority';
 
 // connects our back end code with the database
 mongoose.connect(dbRoute, { useNewUrlParser: true });
